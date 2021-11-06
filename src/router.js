9aa2db5438ce89b7dc09831d64fbbc71aeaf58a6
Giuseppe256 <massaro.jp256@gmail.com>
2021-11-06 18:21:35 -0500

    Updated things

--- src/router.js
+++ src/router.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import Projects from '@/components/Projects.vue'
 import Contact from '@/components/Contact.vue'
 
-Vue.use(Router);
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes: [{
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/Home',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/Projects',
+            name: 'Projects',
+            component: Projects
+        },
+        {
+            path: '/Contact',
+            name: 'Contact',
+            component: Contact
+        }
+    ]
+  })
+
+/*Vue.use(Router);
 
 export default new Router({
     mode: 'history',
@@ -31,3 +56,4 @@ export default new Router({
         }
     ]
 });
+*/
