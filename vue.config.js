97de1132335dbd3d0976881b451934e4a903543b
Giuseppe256 <massaro.jp256@gmail.com>
2021-11-15 17:19:01 -0600

    Added sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,28 @@
 module.exports = {
-    chainWebpack: config => {
-      config.resolve.alias.set('vue', '@vue/compat')
-  
-      config.module
-        .rule('vue')
-        .use('vue-loader')
-        .tap(options => {
-          return {
-            ...options,
-            compilerOptions: {
-              compatConfig: {
-                MODE: 2
-              }
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        'https://joemassaro.studio/',
+        'https://joemassaro.studio/projects',
+        'https://joemassaro.studio/contact'
+      ]
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
             }
           }
-        })
-      }
+        }
+      })
+    }
   }
